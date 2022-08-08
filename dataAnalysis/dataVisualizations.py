cbe1b9ad701cdf56fa15b3f5eb999e8ccec8e71a
TeeronHT <teeron@hajebi.net>
2022-08-08 16:43:58 -0700

    Creating the Pie Chart of risk

--- dataAnalysis/dataVisualizations.py
+++ dataAnalysis/dataVisualizations.py
@@ -19,8 +19,10 @@ with open("../data/RiskData.csv", 'r') as csvfile:
             MediumRisk = MediumRisk + 1
         elif row[1] == '10':
             HighRisk = HighRisk + 1
+        elif row[1] == '15':
+            ExtremeRisk = ExtremeRisk + 1
             
-riskDict = {'No Risk': NoRisk, 'Low Risk': LowRisk, 'Medium Risk': MediumRisk, 'High Risk': HighRisk}
+riskDict = {'No Risk': NoRisk, 'Low Risk': LowRisk, 'Medium Risk': MediumRisk, 'High Risk': HighRisk, 'ExtremeRisk': ExtremeRisk}
         
 labels = []
 data = []
@@ -31,9 +33,9 @@ for riskLevel, riskFrequency in riskDict.items():
     
 sns.set_style('whitegrid')
 plt.rcParams['text.color'] = 'black'
-explode = (0, 0, 0, 0.1)
+explode = (0, 0, 0, 0.1, 0)
 
-colors = sns.color_palette('bright')[4:len(riskDict) + 4]
+colors = sns.color_palette('bright')[4:len(riskDict) + 5]
 plt.pie(data, explode=explode, labels=labels, colors=colors, startangle=45)
 plt.title('Frequency of Risk Levels', fontweight='bold')
 
