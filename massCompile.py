d9a28eb2ebc8bb82227ba31ea6f58767b7ebad6b
TeeronHT <teeron@hajebi.net>
2022-08-08 16:43:17 -0700

    Adding the Goerli contracts to the ContractData.json dataset

--- massCompile.py
+++ massCompile.py
@@ -8,29 +8,38 @@ def main():
 	# Not all of these work when imported into Retool for some reason
 	# 'gem', 'curv'
 
-	with open('./contracts/EthereumMainnetContracts.json') as file:
+	with open('./contracts/GoerliContracts.json') as file:
 		contractData = list(file)
 
 	contracts = []
 	for i in range(len(contractData)):
-		for key in portalDApps:
-			firstN = contractData[i][contractData[i].index("name") + 8:]
-			name = firstN[:firstN.index("\"")]
+		if "name" in contractData[i]:
+			for key in portalDApps:
+				firstN = contractData[i][contractData[i].index("name") + 8:]
+				name = firstN[:firstN.index("\"")]
 
-			firstA = contractData[i][contractData[i].index("address") + 11:]
-			address = firstA[:firstA.index("\"")]
-			if key.replace(" ", "").upper() in name.upper():
-				contractList = {}
-				if 'V1' not in key.upper() and 'V2' not in key.upper():
-					if (TRMVerification(address, 'ethereum') == True):
-						contractList['contractAddress'] = address
-						contractList['dAppName'] = key
-						contractList['contractUrl'] = portalDApps[key]
-						contractList['networkName'] = "EthereumMainnet"
-						contracts.append(contractList)
+				firstA = contractData[i][contractData[i].index("address") + 11:]
+				address = firstA[:firstA.index("\"")]
+
+				overlap = False
+				for contract in contracts:
+					if contract["contractAddress"] == address:
+						overlap = True
+
+				if overlap == False:
+					if key.replace(" ", "").upper() in name.upper():
+						contractList = {}
+						if 'V1' not in key.upper() and 'V2' not in key.upper():
+							#if (TRMVerification(address, 'ethereum') == True):
+							contractList['contractAddress'] = address
+							contractList['dAppName'] = key
+							contractList['contractUrl'] = portalDApps[key]
+							contractList['networkName'] = "Goerli"
+							contracts.append(contractList)
 
 	with open('./data/ContractData.json', 'w') as outfile:
-		json.dump(contracts, outfile)
+		outfile.update(contracts)
+		# json.dump(contracts, outfile)
 
 def TRMVerification(address, network):
 	url = "https://api.trmlabs.com/public/v2/screening/addresses"
