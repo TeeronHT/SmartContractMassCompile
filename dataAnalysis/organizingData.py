168c5f399f891a94c85361f7eda681a1b85edaa8
TeeronHT <teeron@hajebi.net>
2022-08-12 17:06:24 -0700

    Setting up category risk data

--- dataAnalysis/organizingData.py
+++ dataAnalysis/organizingData.py
@@ -1,70 +1,115 @@
 import csv
 import json
 
-file = open("../data/RiskData.csv")
-csvFile = csv.reader(file)
-next(csvFile)
 
-dApps = []
-dAppRisk = {}
+def main():
+	contracts_with_risk()
+	category_risk()
 
-data = []
+def category_risk():
+	file = open("../data/ContractData.json")
+	data = json.load(file)
 
-for line in csvFile:
-	if line[2] not in dApps:
-		dApps.append(line[2])
-	data.append(line)
+	categories = []
+	alreadyListed = []
+	for contract in data: 
+		unlisted = True
+		dAppCategories = contract["risks"]
+		if dAppCategories == None:
+			continue
+		for key, value in dAppCategories.items():
+			for category in categories:
+				if key == category["category"]:
+					unlisted = False
+					category["frequency"] = category["frequency"] + 1
+					category["total risk"] = category["total risk"] + value
 
-for dApp in dApps:
-	totalRisk = {}
-	totalRisk['Total Low Risk Contracts'] = 0
-	totalRisk['Total Medium Risk Contracts'] = 0
-	totalRisk['Total High Risk Contracts'] = 0
-	totalRisk['Total Extreme Risk Contracts'] = 0
-	dAppRisk[dApp] = totalRisk
+			risk = {}
+			if unlisted == True:
+				alreadyListed.append(key)
+				risk["category"] = key
+				risk["frequency"] = 1
+				risk["total risk"] = value
+				risk["average risk"] = risk["total risk"] / risk["frequency"]
+				categories.append(risk)
+			unlisted = True
 
-for dApp in data:
-	if dApp[1] == '1':
-		dAppRisk[dApp[2]]['Total Low Risk Contracts'] = dAppRisk[dApp[2]]['Total Low Risk Contracts'] + 1
-	elif dApp[1] == '5':
-		dAppRisk[dApp[2]]['Total Medium Risk Contracts'] = dAppRisk[dApp[2]]['Total Medium Risk Contracts'] + 1
-	elif dApp[1] == '10':
-		dAppRisk[dApp[2]]['Total High Risk Contracts'] = dAppRisk[dApp[2]]['Total High Risk Contracts'] + 1
-	elif dApp[1] == '15':
-		dAppRisk[dApp[2]]['Total Extreme Risk Contracts'] = dAppRisk[dApp[2]]['Total Extreme Risk Contracts'] + 1
+	dataFile = open('../data/CategoryRisk.csv', 'w')
+	csvWriter = csv.writer(dataFile)
 
-dataFile = open('../data/DAppRisk.csv', 'w')
-csvWriter = csv.writer(dataFile)
+	headers = categories[0].keys()
 
-row = []
-rows = []
-for key in dAppRisk:
-	row.append(dAppRisk[key]['Total Low Risk Contracts'])
-rows.append(row)
+	rows = []
+	for category in categories:
+		rows.append(category.values())
+	print(rows)
 
-row = []
-for key in dAppRisk:
-	row.append(dAppRisk[key]['Total Medium Risk Contracts'])
-rows.append(row)
+	csvWriter.writerow(headers)
+	csvWriter.writerows(rows)
 
-row = []
-for key in dAppRisk:
-	row.append(dAppRisk[key]['Total High Risk Contracts'])
-rows.append(row)
+def contracts_with_risk():
+	file = open("../data/RiskData.csv")
+	csvFile = csv.reader(file)
+	next(csvFile)
 
-row = []
-for key in dAppRisk:
-	row.append(dAppRisk[key]['Total Extreme Risk Contracts'])
-rows.append(row)
-print(rows)	
+	dApps = []
+	dAppRisk = {}
+	data = []
 
- 
-header = dApps
-csvWriter.writerow(header)
+	for line in csvFile:
+		if line[2] not in dApps:
+			dApps.append(line[2])
+		data.append(line)
 
-csvWriter.writerows(rows)
+	for dApp in dApps:
+		totalRisk = {}
+		totalRisk['Total Low Risk Contracts'] = 0
+		totalRisk['Total Medium Risk Contracts'] = 0
+		totalRisk['Total High Risk Contracts'] = 0
+		totalRisk['Total Extreme Risk Contracts'] = 0
+		dAppRisk[dApp] = totalRisk
 
-#print(dAppRisk)
+	for dApp in data:
+		if dApp[1] == '1':
+			dAppRisk[dApp[2]]['Total Low Risk Contracts'] = dAppRisk[dApp[2]]['Total Low Risk Contracts'] + 1
+		elif dApp[1] == '5':
+			dAppRisk[dApp[2]]['Total Medium Risk Contracts'] = dAppRisk[dApp[2]]['Total Medium Risk Contracts'] + 1
+		elif dApp[1] == '10':
+			dAppRisk[dApp[2]]['Total High Risk Contracts'] = dAppRisk[dApp[2]]['Total High Risk Contracts'] + 1
+		elif dApp[1] == '15':
+			dAppRisk[dApp[2]]['Total Extreme Risk Contracts'] = dAppRisk[dApp[2]]['Total Extreme Risk Contracts'] + 1
+
+	dataFile = open('../data/DAppRisk.csv', 'w')
+	csvWriter = csv.writer(dataFile)
+
+	row = []
+	rows = []
+	for key in dAppRisk:
+		row.append(dAppRisk[key]['Total Low Risk Contracts'])
+	rows.append(row)
+
+	row = []
+	for key in dAppRisk:
+		row.append(dAppRisk[key]['Total Medium Risk Contracts'])
+	rows.append(row)
+
+	row = []
+	for key in dAppRisk:
+		row.append(dAppRisk[key]['Total High Risk Contracts'])
+	rows.append(row)
+
+	row = []
+	for key in dAppRisk:
+		row.append(dAppRisk[key]['Total Extreme Risk Contracts'])
+	rows.append(row)
+	print(rows)	
+
+	header = dApps
+	csvWriter.writerow(header)
+	csvWriter.writerows(rows)
+
+if __name__ == "__main__":
+	main()
 
 
-# TODO: find the percent of each dApps contracts that are of each risk level (histogram)
+
